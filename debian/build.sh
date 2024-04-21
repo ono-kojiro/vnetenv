40cce5c02be728b54a9f5e813dcd4f38c554bc93
Kojiro ONO <ono.kojiro@gmail.com>
2024-04-21 16:09:54 +0900

    debian on bhyve

---
+++ debian/build.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+top_dir="$(cd "$(dirname "$0")" > /dev/null 2>&1 && pwd)"
+cd $top_dir
+
+template="debian"
+name="debian"
+iso="debian-12.5.0-amd64-netinst.iso"
+
+segs="40 50 60"
+
+all()
+{
+  help
+}
+
+help()
+{
+  cat - << EOF
+usage : $0 target1 target2 ...
+
+    target
+      install
+EOF
+}
+
+install()
+{
+  if [ ! -e "/vm/.iso/$iso" ]; then
+    sudo vm iso $HOME/Downloads/$iso
+  fi
+
+  cat - << EOF > _tmp.conf
+loader="grub"
+cpu=2
+memory=512M
+network0_type="virtio-net"
+network0_switch="br0"
+disk0_type="ahci-hd"
+disk0_name="disk0.img"
+grub_run_partition="1"
+grub_run_dir="/boot/grub"
+EOF
+
+  sudo cp -f _tmp.conf /vm/.templates/${template}.conf
+  rm -f _tmp.conf
+
+  sudo vm create -t ${template} -s 10g -m 512m -c 2 ${name}
+  sudo vm install ${name} ${iso}
+
+  sleep 3
+
+  sudo vm console ${name}
+}
+
+hosts()
+{
+  ansible-inventory -i template.yml --yaml --list --output hosts.yml
+}
+
+prepare()
+{
+  ssh netbsd pkgin install python311
+}
+
+default()
+{
+  tag=$1
+  ansible-playbook -i hosts.yml -t ${tag} site.yml
+}
+
+clone()
+{
+  for seg in $segs; do
+	vmname="${name}${seg}"
+    sudo vm clone ${name} ${vmname}
+	config="/vm/${vmname}/${vmname}.conf"
+	sudo sed -i '' -e "s/\"br0\"/\"br${seg}\"/" $config
+  done
+}
+
+unclone()
+{
+  for seg in $segs; do
+	vmname="${name}${seg}"
+    sudo vm destroy -f ${vmname}
+  done
+}
+
+startall()
+{
+  for seg in $segs; do
+	vmname="${name}${seg}"
+    sudo vm start ${vmname}
+  done
+}
+
+stopall()
+{
+  for seg in $segs; do
+	vmname="${name}${seg}"
+    sudo vm stop ${vmname}
+  done
+}
+
+
+hosts
+
+if [ $# -eq 0 ]; then
+  all
+fi
+
+for target in "$@"; do
+  LANG=C type "$target" 2>&1 | grep 'function' > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    $target
+  else
+    default $target
+  fi
+done
+
+
