70f747404d9902c245065bc02865bca1510319c1
Kojiro ONO <ono.kojiro@gmail.com>
2024-05-03 21:19:12 +0900

    refactoring agent.py

--- opnsense/snmp/build.sh
+++ opnsense/snmp/build.sh
@@ -22,6 +22,7 @@ target:
   dot
   png
   svg
+  pdf
 EOF
 }
 
@@ -30,11 +31,9 @@ all()
   snmp
   json
   db
-  analysis
-  dot
+  #analysis
   dot2
-  png
-  svg
+  pdf
 }
 
 snmp()
@@ -156,6 +155,16 @@ svg()
   echo $cmd
   $cmd
   cmd="command dot -Tsvg -o mygraph2.svg mygraph2.dot"
+  echo $cmd
+  $cmd
+}
+
+pdf()
+{
+  cmd="command dot -Tpdf -o mygraph.pdf  mygraph.dot"
+  echo $cmd
+  $cmd
+  cmd="command dot -Tpdf -o mygraph2.pdf mygraph2.dot"
   echo $cmd
   $cmd
 }
