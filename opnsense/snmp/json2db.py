52742e745e7c5a89f007a3a3ef85201c09ff0bd6
Kojiro ONO <ono.kojiro@gmail.com>
2024-05-07 22:37:07 +0900

    update ifname_table

--- opnsense/snmp/json2db.py
+++ opnsense/snmp/json2db.py
@@ -95,8 +95,8 @@ def create_ifname_table(conn):
     sql = 'CREATE TABLE {0} ('.format(table)
     sql += 'id INTEGER PRIMARY KEY, '
     sql += 'sysname TEXT, '
-    sql += 'ifid INTEGER, '
-    sql += 'ifname TEXT '
+    sql += 'ifname TEXT, '
+    sql += 'ifid INTEGER '
     sql += ');'
 
     c.execute(sql)
@@ -266,15 +266,15 @@ def create_segment_view(conn):
 
     c.execute(sql)
 
-def insert_ifname(conn, sysname, ifid, ifname):
+def insert_ifname(conn, sysname, ifname, ifid):
     table = 'ifname_table'
 
     c = conn.cursor()
     sql = 'INSERT INTO {0} VALUES ( NULL, ?, ?, ?);'.format(table)
     item = [
         sysname,
-        ifid,
         ifname,
+        ifid,
     ]
 
     c.execute(sql, item)
@@ -402,29 +402,31 @@ def search_mac(conn, data, sysname, ifmacs):
 
                 insert_mac(conn, sysname, ifid, ip, mac)
 
-def search_sysname(conn, data):
+def search_sysname(data):
     keyword = "['sysName.0']"
     expr = parse('$..' + keyword)
 
-    sysname = None
-
+    items = []
+    
     for m in expr.find(data):
         tree = m.value
-
         sysname = tree['val']
-        insert_sysname(conn, sysname)
+        items.append(sysname)
 
-    return sysname
+    return items
 
-def search_ifname(conn, data, sysname):
+def search_ifname(conn, data):
     keyword = 'ifName'
     expr = parse('$..' + keyword)
+
+    items = {}
 
     for m in expr.find(data):
         tree = m.value
         for ifid in tree:
             ifname = tree[ifid]['val']
-            insert_ifname(conn, sysname, ifid, ifname)
+            items[ifname] = ifid
+    return items
 
 def main():
     ret = 0
@@ -464,41 +466,26 @@ def main():
     conn = sqlite3.connect(output)
     create_table(conn)
  
-    records = {}
-
-    keywords = [
-#        "['sysName.0']",
-#        "['ifNumber.0']",
-#        'ifName',
-#        'ifPhysAddress',
-        'ipNetToPhysicalPhysAddress',
-    ]
-
     for filepath in args:
         data = read_json(filepath)
-        sysname = search_sysname(conn, data)
+        sysnames = search_sysname(data)
+        if len(sysnames) != 1 :
+            print('ERROR: no sysname in {0}'.format(filepath))
+            sys.exit(1)
+
+        sysname = sysnames[0]
+        insert_sysname(conn, sysname)
+        
+        # ifnames[ifname] => ifid
+        ifnames = search_ifname(conn, data)
+        for ifname in ifnames:
+            ifid = ifnames[ifname]
+            insert_ifname(conn, sysname, ifname, ifid)
+
         ifmacs = search_ifmac(conn, data, sysname)
         search_mac(conn, data, sysname, ifmacs)
-        search_ifname(conn, data, sysname)
         search_netmask(conn, data)
         search_defaultrouter(conn, data, sysname)
-
-    #yaml.dump(records,
-    #    fp,
-    #    allow_unicode=True,
-    #    default_flow_style=False,
-    #    sort_keys=True
-    #)
-
-    #fp.write(
-    #    json.dumps(
-    #        records,
-    #        indent=4,
-    #        ensure_ascii=False,
-    #        sort_keys=True,
-    #    )
-    #)
-    #fp.write('\n')
 
     create_view(conn)
     conn.commit()
