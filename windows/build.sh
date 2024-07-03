3dc03663a2fe7315209240d97e8d0d98fea7ee64
Kojiro ONO <ono.kojiro@gmail.com>
2024-07-03 22:35:03 +0900

    disable network

--- windows/build.sh
+++ windows/build.sh
@@ -8,7 +8,7 @@ if [ ! -e "/vm/.iso/$iso" ]; then
   sudo vm iso $iso_path
 fi
 
-install()
+create()
 {
   cat - << EOF > _tmp.conf
 loader="uefi"
@@ -16,7 +16,7 @@ graphics="yes"
 graphics_wait="yes"
 xhci_mouse="yes"
 cpu=2
-memory=4G
+memory=4096m
 
 # put up to 8 disks on a single ahci controller.
 # without this, adding a disk pushes the following network devices onto higher slot numbers,
@@ -25,8 +25,8 @@ ahci_device_limit="8"
 
 # ideally this should be changed to virtio-net and drivers installed in the guest
 # e1000 works out-of-the-box
-network0_type="e1000"
-network0_switch="public"
+#network0_type="e1000"
+#network0_switch="public"
 disk0_type="ahci-hd"
 disk0_name="disk0.img"
 
@@ -35,7 +35,11 @@ utctime="no"
 EOF
 
   sudo cp -f _tmp.conf /vm/.templates/windows.conf
-  sudo vm create -t windows -s 32g -m 4g -c 2 $name
+  sudo vm create -t windows -s 32g -m 4096m -c 2 $name
+}
+
+install()
+{
   sudo vm install $name $iso
 }
 
